3c850cc0cc6e2f6d4d090ecd9f23ec3c9c5d0555
elshennawyhamza <elshennawyhamza@gmail.com>
2021-11-10 00:07:41 +0200

    code cleanup

--- library/library.cpp
+++ library/library.cpp
@@ -22,10 +22,6 @@ int login();
 
 
 void WriteToFile(Person pr);
-//bool IsValid(string user, string pass);
-//int Login();
-//void Logout();
-
 
 
 Person ReadFromFile();
@@ -71,66 +67,31 @@ int main()
     return 0;
 }
 
-//int Login()
+//
+//void WriteToFile(Person pr)
 //{
-//    if (IsValid)
-//    {
-//        return 1;
-//    }
-//    return 0;
+//    fstream file_obj;
+//    string PersonAccountType = to_string(pr.getAccountType());
+//    string PersonIDD = to_string(pr.getPersonID());
+//    file_obj.seekg(0);
+//    file_obj.open("test.txt", ios::app);
+//    file_obj.write(pr.getPersonName().c_str(), pr.getPersonName().length() + 1);
+//    file_obj.write(pr.getPersonPassword().c_str(), pr.getPersonPassword().length()+1);
+//    file_obj.close();
+//    
 //}
 //
-//void Logout()
+//Person ReadFromFile()
 //{
-//
+//    Person pr;
+//    fstream file_obj;
+//    file_obj.open("test.txt", ios::in);
+//    file_obj.read((char*)&pr, sizeof(pr));
+//    return pr;
 //}
-
-
-
-void WriteToFile(Person pr)
-{
-    fstream file_obj;
-    string PersonAccountType = to_string(pr.getAccountType());
-    string PersonIDD = to_string(pr.getPersonID());
-    file_obj.seekg(0);
-    file_obj.open("test.txt", ios::app);
-    file_obj.write(pr.getPersonName().c_str(), pr.getPersonName().length() + 1);
-    file_obj.write(pr.getPersonPassword().c_str(), pr.getPersonPassword().length()+1);
-    //file_obj.write(PersonAccountType.c_str(), PersonAccountType.length() + 1);
-    //file_obj.write(PersonIDD.c_str(), PersonIDD.length() + 1);
-    file_obj.close();
-    
-}
-Person ReadFromFile()
-{
-    Person pr;
-    fstream file_obj;
-    file_obj.open("test.txt", ios::in);
-    file_obj.read((char*)&pr, sizeof(pr));
-    return pr;
-}
-
-
-
-//bool IsValid(string user, string pass)
-//{
-//
-//
-//
-//
-//
-//
-//
-//    return true;
-//}
-
-
-
-
 
 int login()
 {
-    int choice, i, exit = 0;
     string  password, name, fileName, usern, pw, AccountLevel;
     ofstream fileo;
     ifstream filei;
